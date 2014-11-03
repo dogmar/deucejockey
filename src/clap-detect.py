34d090cc49e9271449a20e4a4fec3646091374cb
Chris Klink <chris.klink@gmail.com>
2014-11-03 22:37:54 +0000

    Added LCD support

--- src/clap-detect.py
+++ src/clap-detect.py
@@ -1,21 +1,16 @@
 import slowclap as sc
 import httplib
 import sched, time
-
+import math
 import sys
 import optparse
-
 import select
-
 import json
-
 import os
-
 import threading
-
 from collections import deque
 
-THRESHOLD = 5e6
+THRESHOLD = 5e12
 WINDOW =.05
 WAIT=.2
 
@@ -40,7 +35,7 @@ Detect claps on the default microphone."""  # nopep8
                       action='store',
                       type='float',
                       default=WINDOW,
-                      help='Noises over the threshold must be shorter than this time in seconds to count as a clap '
+                      help='Noises over the zz must be shorter than this time in seconds to count as a clap '
                       '[{0}]'.format(WINDOW))
     parser.add_option('-W', '--wait', 
                       action='store',
@@ -61,6 +56,7 @@ class ClapDetector(object):
                                             window=window,
                                             wait=wait)
         self.claps = deque()
+        self.set_sensitivity(.5)
 
     def detect_claps(self):
         print 'detect claps'
@@ -85,6 +81,13 @@ class ClapDetector(object):
         except Exception as e:
             print "Can't start emitter_thread", e
 
+        try:
+            self.lcd_thread = threading.Thread(target=self.lcd_loop)
+            self.lcd_thread.daemon = True
+            self.lcd_thread.start()
+        except Exception as e:
+            print "Can't start lcd_thread", e
+
         self.main_loop()
 
     def start_emitter(self):
@@ -106,15 +109,24 @@ class ClapDetector(object):
         for clap in self.detector:
             self.claps.append(clap)
 
-    def threshold_up(self):
-        self.detector.threshold *= 1.2
-        print 'Threshold:', self.detector.threshold
+    def set_sensitivity(self, sensitivity):
+        if sensitivity > 0.99:
+            sensitivity = 0.99
+        elif sensitivity < 0.00:
+            sensitivity = 0.00
+        self.sensitivity = sensitivity
+
+        threshold = (math.pow(THRESHOLD, (1 - sensitivity)))
+        self.detector.threshold = threshold
+        print 'Threshold:', self.detector.threshold, 'Sensitivity:', self.sensitivity
         sys.stdout.flush()
 
-    def threshold_down(self):
-        self.detector.threshold /= 1.2
-        print 'Threshold:', self.detector.threshold
-        sys.stdout.flush()
+
+    def sensitivity_up(self):
+        self.set_sensitivity(self.sensitivity + .01)
+
+    def sensitivity_down(self):
+        self.set_sensitivity(self.sensitivity - .01)
 
     def read_keyboard(self):
         import termios, fcntl, sys, os
@@ -133,15 +145,51 @@ class ClapDetector(object):
                 try:
                     c = sys.stdin.read(1)
                     if (c == 'a' or c == 'A'):
-                        self.threshold_up()
+                        self.sensitivity_up()
                     elif (c == 'z' or c == 'Z'):
-                        self.threshold_down()
+                        self.sensitivity_down()
                     elif (c == 'c' or c == 'C'):
                         self.emit_clap()
                 except IOError: pass
         finally:
             termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
             fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+    def lcd_loop(self):
+        from time import sleep
+        from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+
+        # Initialize the LCD plate.  Should auto-detect correct I2C bus.  If not,
+        # pass '0' for early 256 MB Model B boards or '1' for all later versions
+        lcd = Adafruit_CharLCDPlate()
+
+        # Clear display and show greeting, pause 1 sec
+        lcd.clear()
+        lcd.message("Deuce Jockey")
+        sleep(1)
+
+        # Cycle through backlight colors
+        col = (lcd.RED , lcd.YELLOW, lcd.GREEN, lcd.TEAL,
+               lcd.BLUE, lcd.VIOLET, lcd.ON   , lcd.OFF)
+        for c in col:
+            lcd.backlight(c)
+            sleep(.1)
+
+        # Poll buttons, display message & set backlight accordingly
+        # lcd.backlight(lcd.RED)
+
+        while True:
+            if lcd.buttonPressed(lcd.UP):
+                print('UP')
+                self.sensitivity_up()
+            elif lcd.buttonPressed(lcd.DOWN):
+                print('DOWN')
+                self.sensitivity_down()
+            if lcd.buttonPressed(lcd.UP) or lcd.buttonPressed(lcd.DOWN):
+                sleep(.1)
+            lcd.clear()
+            lcd.message("Deuce Jockey\nSensitivity: "+repr(round(self.sensitivity * 100)))
+            lcd.backlight(lcd.RED)
 
 
     def main_loop(self):
